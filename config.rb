0f2bd5f76243f69e4e587ca6561116124d0df1ac
Cássio B. Pereira <cassio@cassiobp.com.br>
2023-09-12 20:43:37 +0200

    relative

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'breakpoint'
 
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "/bsideskrakow-2024/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
